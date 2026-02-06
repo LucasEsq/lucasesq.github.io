26b18c87b318d416eeb6344f22f5d2d2d1df0532
LucasEsq <esquenet.lucas@gmail.com>
2026-02-06 22:40:03 +0000

    Get more than 5 stars visible possible

--- components/common/Stars.js
+++ components/common/Stars.js
@@ -1,4 +1,15 @@
 function Stars({ count, max = 5 }) {
+  // If count is greater than max, show as "★×count" format
+  if (count > max) {
+    return (
+      <span className="stars">
+        <span className="star">★</span>
+        <span style={{ marginLeft: '0.25rem', fontWeight: 'bold' }}>×{count}</span>
+      </span>
+    );
+  }
+
+  // Otherwise, show individual stars
   return (
     <span className="stars">
       {[...Array(max)].map((_, i) => (
